79aee33bfa87a756a8b43038ae0fee2a18152bbd
Jumcee <futhmah456@gmail.com>
2024-04-07 05:30:52 +0100

    stakingcontract

--- test/StakingContract.js
+++ test/StakingContract.js
@@ -1,114 +1,108 @@
-const { ethers } = require("ethers");
 const { expect } = require("chai");
-//const { ContractFactory } = ethers;
-
-// Load the contract ABI and bytecode
-// const contractABI = [...]; // Replace with the ABI of your contract
-// const contractBytecode = "0x..."; // Replace with the bytecode of your contract
+const { ethers } = require("ethers");
 
 describe("StakingContract", function () {
   let StakingContract;
-  let hardhatToken;
-  let swapRouterAddress;
+  let stakingContract;
   let owner;
   let addr1;
   let addr2;
-  let addrs;
+  let token;
+  let tokenAddress;
 
   beforeEach(async function () {
-    // Get the ContractFactory and Signers
-    const ERC20Factory = await ethers.getContractFactory("ERC20");
-    hardhatToken = await ERC20Factory.deploy("Hardhat Token", "HHT", 18);
-    await hardhatToken.deployed();
+    // Get the Ethereum provider from ethers
+    const provider = ethers.provider;
 
-    // Use a mock for the UniswapRouter if you don't have the implementation
-    const SwapRouterFactory = await ethers.getContractFactory("MockUniswapRouter");
-    swapRouterAddress = await SwapRouterFactory.deploy();
-    await swapRouterAddress.deployed();
+    // Get the signers from the provider
+    [owner, addr1, addr2] = await provider.getSigners();
 
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    // Declare initialSupply here
+    const initialSupply = ethers.utils.parseEther("1000000");
 
-    const StakingContractFactory = await ethers.getContractFactory("StakingContract");
-    StakingContract = await StakingContractFactory.deploy(
-      swapRouterAddress.address,
-      [hardhatToken.address]
-    );
-    await StakingContract.deployed();
+    // Deploy Token contract
+    const Token = await ethers.getContractFactory("YourTokenContract");
+    token = await Token.deploy(initialSupply);
+    tokenAddress = token.address;
+
+    // Deploy StakingContract
+    const StakingContract = await ethers.getContractFactory("StakingContract");
+    stakingContract = await StakingContract.deploy(owner.address, [tokenAddress]);
+  });
+
+  it("Should deposit tokens", async function () {
+    const depositAmount = ethers.utils.parseEther("10");
+
+    // Transfer tokens to addr1
+    await token.transfer(addr1.address, depositAmount);
+
+    // Approve StakingContract to spend tokens on behalf of addr1
+    await token.connect(addr1).approve(stakingContract.address, depositAmount);
+
+    // Deposit tokens to StakingContract
+    await stakingContract.connect(addr1).deposit(depositAmount);
+
+    // Check staked balances
+    expect(await stakingContract.stakedBalances(addr1.address)).to.equal(depositAmount);
   });
 
 
-  describe("Deployment", function () {
-    it("Should set the right swap router address", async function () {
-      expect(await StakingContract.swapRouterAddress()).to.equal(
-        swapRouterAddress.address
-      );
-    });
+  it("Should withdraw tokens", async function () {
+    const depositAmount = ethers.utils.parseEther("10");
+    const withdrawAmount = ethers.utils.parseEther("5");
 
-    it("Should set the right tokens to swap", async function () {
-      expect(await StakingContract.tokensToSwap(0)).to.equal(hardhatToken.address);
-    });
+    // Transfer tokens to addr1
+    await token.transfer(addr1.address, depositAmount);
+
+    // Approve StakingContract to spend tokens on behalf of addr1
+    await token.connect(addr1).approve(stakingContract.address, depositAmount);
+
+    // Deposit tokens to StakingContract
+    await stakingContract.connect(addr1).deposit(depositAmount);
+
+    // Withdraw tokens from StakingContract
+    await stakingContract.connect(addr1).withdraw(withdrawAmount);
+
+    // Check staked balances
+    expect(await stakingContract.stakedBalances(addr1.address)).to.equal(depositAmount.sub(withdrawAmount));
   });
 
-  describe("Deposit", function () {
-    it("Should deposit tokens", async function () {
-      const amount = ethers.utils.parseEther("100");
-      await hardhatToken.approve(StakingContract.address, amount);
-      await StakingContract.connect(addr1).deposit(amount);
-      expect(await StakingContract.stakedBalances(addr1.address)).to.equal(amount);
-    });
+  it("Should add rewards", async function () {
+    const rewardAmount = ethers.utils.parseEther("100");
+
+    // Transfer tokens to StakingContract
+    await token.transfer(stakingContract.address, rewardAmount);
+
+    // Add rewards to StakingContract
+    await stakingContract.addRewards(rewardAmount);
+
+    // Check rewards balance
+    expect(await token.balanceOf(stakingContract.address)).to.equal(rewardAmount);
   });
 
-  describe("Withdraw", function () {
-    it("Should withdraw tokens", async function () {
-      const amount = ethers.utils.parseEther("100");
-      await hardhatToken.approve(StakingContract.address, amount);
-      await StakingContract.connect(addr1).deposit(amount);
-      await StakingContract.connect(addr1).withdraw(amount.div(2));
-      expect(await StakingContract.stakedBalances(addr1.address)).to.equal(
-        amount.div(2)
-      );
-    });
+  it("Should claim rewards", async function () {
+    const depositAmount = ethers.utils.parseEther("10");
+    const rewardAmount = ethers.utils.parseEther("100");
+
+    // Transfer tokens to addr1
+    await token.transfer(addr1.address, depositAmount);
+
+    // Approve StakingContract to spend tokens on behalf of addr1
+    await token.connect(addr1).approve(stakingContract.address, depositAmount);
+
+    // Deposit tokens to StakingContract
+    await stakingContract.connect(addr1).deposit(depositAmount);
+
+    // Transfer rewards to StakingContract
+    await token.transfer(stakingContract.address, rewardAmount);
+
+    // Claim rewards from StakingContract
+    await stakingContract.connect(addr1).claimRewards();
+
+    // Check rewards balance after claiming
+    expect(await token.balanceOf(addr1.address)).to.equal(rewardAmount);
   });
 
-  describe("Claim Rewards", function () {
-    it("Should claim rewards", async function () {
-      const amount = ethers.utils.parseEther("100");
-      await hardhatToken.transfer(StakingContract.address, amount);
-      await StakingContract.addRewards(amount);
-      await StakingContract.connect(addr1).deposit(ethers.utils.parseEther("1"));
-      await StakingContract.connect(addr1).claimRewards();
-      expect(await hardhatToken.balanceOf(addr1.address)).to.be.gt(0);
-    });
-  });
+  // Add more test cases as needed
 
-  describe("Swap Tokens", function () {
-    it("Should swap tokens", async function () {
-      const ERC20Factory = await ethers.getContractFactory("ERC20Mock");
-      const token2 = await ERC20Factory.deploy("Token 2", "TKN2", 18);
-      await token2.deployed();
-
-      const amount = ethers.utils.parseEther("100");
-      await hardhatToken.transfer(StakingContract.address, amount);
-      await StakingContract.swapTokens(amount, hardhatToken.address, token2.address);
-      expect(await token2.balanceOf(StakingContract.address)).to.be.gt(0);
-    });
-  });
-
-  describe("Add Rewards", function () {
-    it("Should add rewards", async function () {
-      const amount = ethers.utils.parseEther("100");
-      await hardhatToken.approve(StakingContract.address, amount);
-      await StakingContract.addRewards(amount);
-      expect(await StakingContract.rewards(StakingContract.address)).to.equal(amount);
-    });
-  });
-
-  describe("Withdraw Excess Tokens", function () {
-    it("Should withdraw excess tokens", async function () {
-      const amount = ethers.utils.parseEther("100");
-      await hardhatToken.transfer(StakingContract.address, amount);
-      await StakingContract.withdrawExcessTokens(hardhatToken.address, amount);
-      expect(await hardhatToken.balanceOf(owner.address)).to.equal(amount);
-    });
-  });
 });
